c7ade5171d9dfabc7be3fc35b11f4ac0258530ce
Mahbub2027 <alam201127@gmail.com>
2023-09-14 14:16:32 +0600

    title updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Countries from './components/Countries/Countries'
 function App() {
   return (
     <>
-      <h2>React FANTASY World Tour</h2> 
+      <h2>React World Tour</h2> 
       <Countries></Countries>
     </>
   )
